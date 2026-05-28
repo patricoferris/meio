b4808f4934111681be412a42afdcd9f2bffd8779
Patrick Ferris <patrick@sirref.org>
2026-05-28 09:42:42 +0100

    Use Cmdliner and remove eio_main dependency

--- src/bin/meio.ml
+++ src/bin/meio.ml
@@ -1,7 +1,5 @@
-let run _stdenv exec_args =
-  let argsl = String.split_on_char ' ' exec_args in
-  let executable_filename = List.hd argsl in
-  let argsl = Array.of_list argsl in
+let run (exec, args) =
+  let argsl = Array.of_list args in
   let tmp_dir = Filename.get_temp_dir_name () in
   let env =
     Array.append
@@ -16,7 +14,7 @@ let run _stdenv exec_args =
     Unix.openfile Filename.null [ Unix.O_WRONLY; Unix.O_KEEPEXEC ] 0o666
   in
   let child_pid =
-    Unix.create_process_env executable_filename argsl env Unix.stdin dev_null
+    Unix.create_process_env exec argsl env Unix.stdin dev_null
       dev_null
   in
   Unix.sleepf 0.2;
@@ -29,4 +27,31 @@ let run _stdenv exec_args =
   in
   Unix.unlink ring_file
 
-let () = Eio_main.run @@ fun stdenv -> run stdenv Sys.argv.(1)
+open Cmdliner
+
+let executable =
+  let doc = "The executable to run and monitor." in
+  Arg.(required & pos 0 (some string) None & info [] ~doc ~docv:"EXECUTABLE")
+
+let args =
+  let doc = "Arguments for the executable to monitor." in
+  Arg.(value & pos_right 1 string [] & info [] ~doc ~docv:"ARGS")
+
+let run_cmd =
+  let open Cmdliner.Term.Syntax in
+  let doc = "Monitor Eio programs from the terminal" in
+  let man = [
+    `S Manpage.s_examples;
+    `P "If your Eio program does not take arguments, or if all the arguments\
+        are positional, run commands with `$(cmd) <executable> <arg1> <arg2> ...'";
+    `P "If your Eio program needs flags then use `--' disambiguation, e.g., `$(cmd) -- <executable> --flag1 --flag2=foo ...'";
+    `S Manpage.s_bugs;
+    `P "Send bug reports to https://github.com/tarides/meio." ]
+  in
+  Cmd.make (Cmd.info "meio" ~version:"%%VERSION%%" ~doc ~man) @@
+  let+ executable and+ args in
+  run (executable, args)
+
+
+let main () = Cmd.eval run_cmd 
+let () = if !Sys.interactive then () else exit (main ())
